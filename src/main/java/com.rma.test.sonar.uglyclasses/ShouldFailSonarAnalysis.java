e53c98cb15598a71afa0ddefc0b3088d3617d6c6
Adam Korynta <adam@rmanet.com>
2019-02-22 08:45:09 -0800

    first commit

---
+++ src/main/java/com.rma.test.sonar.uglyclasses/ShouldFailSonarAnalysis.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019
+ * United States Army Corps of Engineers - Hydrologic Engineering Center (USACE/HEC)
+ * All Rights Reserved.  USACE PROPRIETARY/CONFIDENTIAL.
+ * Source may not be released without written approval from HEC
+ */
+
+package com.rma.test.sonar.uglyclasses;
+
+/**
+ * Company: Resource Management Associates
+ *
+ * @author <a href="mailto:adam@rmanet.com">Adam Korynta</a>
+ * @since 02-22-2019
+ */
+public class ShouldFailSonarAnalysis
+{
+	
+}
